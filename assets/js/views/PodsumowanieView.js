9b69a9f284324dd087b6eec3d83f0d17dce8c26e
Slaszu <slaszu@op.pl>
2013-06-10 22:45:29 +0200

    summary in progress (done : fuel consumption , mileage)

--- assets/js/views/PodsumowanieView.js
+++ assets/js/views/PodsumowanieView.js
@@ -45,17 +45,38 @@
         render: function() {
 			
 			var dane = {};
-			var coll = this.collection.toJSON();			
-			// spalanie : srednie, najwyzsze, najnizsze
-			dane.spalanie = {};
-			dane.spalanie.max = _.max(coll, function(one){ if (one.spalanie != "") return one.spalanie; }).spalanie.toFixed(2);
-			dane.spalanie.min = _.min(coll, function(one){ if (one.spalanie != "") return one.spalanie; }).spalanie.toFixed(2);
+			dane.spalanie 	= {'max': 0, 'min': 0, 'avg': 0};
+			dane.przebieg 	= {'max': 0, 'min': 0, 'all': 0, 'between': 0, 'between_max':0 , 'between_min':0};
+			dane.paliwo 	= {'sum': 0};
 			
-			var spalanieWsk = 0;
-			var spalanieSum = _.reduce(coll, function(memo, one){ if (one.spalanie != "") {spalanieWsk++; memo = memo + one.spalanie;} return memo; }, 0);
-			dane.spalanie.avg = (spalanieSum/spalanieWsk).toFixed(2);
+			var coll = this.collection.toJSON();
+			
+			if (coll.length > 0) {
+			
+				dane.spalanie.max 	= _.max(coll, function(one){ if (one.spalanie != "") return one.spalanie; }).spalanie.toFixed(2);
+				dane.spalanie.min 	= _.min(coll, function(one){ if (one.spalanie != "") return one.spalanie; }).spalanie.toFixed(2);
+				
+				dane.przebieg.min 	= _.min(coll, function(one){ return one.przebieg; }).przebieg;
+				dane.przebieg.max 	= _.max(coll, function(one){ return one.przebieg; }).przebieg;
+				dane.przebieg.all 	= dane.przebieg.max - dane.przebieg.min;
+				dane.przebieg.between 		= Math.round((dane.przebieg.all / coll.length));
+				var between = [];
+				_.reduce(coll.reverse(), function (memo, one) { var x = one.przebieg - memo; memo = one.przebieg; between.push(x); return memo;}, 0);
+				console.log(between);
+				dane.przebieg.between_max 	= _.max(between);
+				dane.przebieg.between_min 	= _.min(between);
+				
+				dane.paliwo.sum		= _.reduce(coll, function (memo, one) { return memo + one.ilosc}, 0);
+				
+				// srednie spalanie to ( ilosc calego paliwa - ostatnie tankowanie * 100) / max przebieg - min przebieg
+				dane.spalanie.avg	= ((dane.paliwo.sum - coll[0].ilosc) * 100)/ (dane.przebieg.all);
+				dane.spalanie.avg 	= dane.spalanie.avg.toFixed(2);
+				
+			}
 			
 			console.log(dane);
+			
+			
 			
 			this.template = _.template( $( "script#podsumowanieContent" ).html(), {'dane' : dane} );
 			this.$el.html(this.template);
